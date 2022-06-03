29be7592cf4dadcdb452b5912aed3ac9af0ac3a8
HADI <hadiswaleh887@gmail.com>
2022-06-03 16:20:10 +0500

    Signed-off-by: HADI

---
+++ src/pages/dashboard/onenews.js
@@ -0,0 +1,61 @@
+import {Typography} from "@mui/material";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import Comment from "../../component/comment";
+import AcccessibleTable from "../../component/table";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import NewsCard from "../../component/newcard";
+
+
+
+
+
+function Onenews(){
+
+const [data,setData] = useState([]);
+const location = useLocation();
+const { source , publishedAt , content , urlToImage , title} = location.state
+const navigate = useNavigate();
+
+
+useEffect(() => {
+
+  if(location.state){
+   // console.log(location.state)
+    // navigate('/dashboard/onenews')
+  }else{
+    
+    navigate('/')
+    //console.log(location.state)
+  }
+
+},[])
+
+
+
+return <>
+<div className="">
+
+   
+<div className="row mt-4">
+        <div className="col-12">
+           <div className="table-responsive">
+           {/* <AcccessibleTable rows={rows} /> */}
+           </div>
+        </div>
+    </div>
+
+
+    <div className="row mt-4">
+          <NewsCard   source={source} publishedAt={publishedAt} content={content} urlToImage={urlToImage} title={title} />
+    </div>
+   
+   
+</div>
+ 
+
+
+</>
+}
+
+export default Onenews;
